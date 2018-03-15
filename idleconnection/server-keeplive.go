c233145470c060cc12e4066fe2cb6b27cf59a70b
yqsy <yqsy021@126.com>
2018-03-15 16:04:14 +0800

    :trollface: keeplive

---
+++ idleconnection/server-keeplive.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"net"
+	"time"
+	"io"
+	"log"
+)
+
+func panicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func serverConn(conn net.Conn) {
+	defer conn.Close()
+
+	conn.(*net.TCPConn).SetKeepAlive(true)
+	conn.(*net.TCPConn).SetKeepAlivePeriod(3 * time.Second)
+
+	io.Copy(conn, conn)
+
+	log.Printf("%v -> %v stoped\n", conn.RemoteAddr(), conn.LocalAddr())
+}
+
+func main() {
+
+	arg := os.Args
+	if len(arg) < 2 {
+		fmt.Printf("Usage:\n %v listenaddr\nExample:\n %v :10001\n", arg[0], arg[0])
+		return
+	}
+
+	listener, err := net.Listen("tcp", arg[1])
+	panicOnError(err)
+
+	defer listener.Close()
+
+	for {
+		localConn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		go serverConn(localConn)
+	}
+}
