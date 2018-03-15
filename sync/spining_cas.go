ccba980a80ebbbe7b75a2534e7248c440980bd74
yqsy <yqsy021@126.com>
2018-03-15 17:25:43 +0800

    :alien:

---
+++ sync/spining_cas.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"sync/atomic"
+	"runtime"
+	"time"
+	"fmt"
+)
+
+type SpinLock struct {
+	state *int32
+}
+
+func New() SpinLock {
+	return SpinLock{state: new(int32)}
+}
+
+const free = int32(0)
+
+func (l *SpinLock) Lock() {
+	for !atomic.CompareAndSwapInt32(l.state, free, 42) {
+		runtime.Gosched()
+	}
+}
+
+func (l *SpinLock) Unlock() {
+	atomic.StoreInt32(l.state, free)
+}
+
+func main() {
+	spinLock := New()
+
+	locked := make(chan bool, 1)
+
+	go func(spinLock SpinLock, locked chan bool) {
+		spinLock.Lock()
+		locked <- true
+		time.Sleep(time.Second * 5)
+		spinLock.Unlock()
+	}(spinLock, locked)
+
+	<-locked
+	spinLock.Lock()
+	fmt.Println("done")
+}
