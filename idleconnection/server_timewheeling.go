dd176c2be9d35156ab7dea4b6357045a06731d30
yqsy <yqsy021@126.com>
2018-03-16 15:16:15 +0800

    可以用指针对比了,不过要修改task变量名子

--- idleconnection/server_timewheeling.go
+++ idleconnection/server_timewheeling.go
@@ -7,14 +7,32 @@ import (
 	"fmt"
 )
 
+//  map[ele]struct{} like c++'s unordered_set
+type Bucket struct {
+	tasks map[interface{}]struct{}
+}
+
+func newBucket() *Bucket {
+	bucket := Bucket{}
+	bucket.tasks = make(map[interface{}]struct{})
+	return &bucket
+}
+
+func (bucket *Bucket) deleteTask(task interface{}) {
+	delete(bucket.tasks, task)
+}
+
+func (bucket *Bucket) addTask(task interface{}) {
+	bucket.tasks[bucket] = struct{}{}
+}
+
 type TimeWheel struct {
-	// ring[bucket,bucket,bucket,...]
-	// bucket -> set[task,task,task,...] -> map[task]struct{}{}
+	// ring[bucket*,bucket*,bucket*,...]
 	slots *ring.Ring
 
 	// the bucket of the ring which specify task last added to
-	// map[task]bucket
-	lastBucket map[interface{}]interface{}
+	// map[task]bucket*
+	lastBucket map[interface{}]*Bucket
 
 	durationPerTick time.Duration
 
@@ -36,7 +54,7 @@ func New(ticksPerWheel int, durationPerTick time.Duration, f func(interface{})) 
 
 	tw := &TimeWheel{
 		slots:           ring.New(ticksPerWheel),
-		lastBucket:      make(map[interface{}]interface{}),
+		lastBucket:      make(map[interface{}]*Bucket),
 		durationPerTick: durationPerTick,
 		addChan:         make(chan interface{}),
 		delChan:         make(chan interface{}),
@@ -49,9 +67,9 @@ func New(ticksPerWheel int, durationPerTick time.Duration, f func(interface{})) 
 
 	slotsLen := tw.slots.Len()
 
-	// init slot's each bucket with set[task,task,task...]
+	// init slot's each bucket with Bucket
 	for i := 0; i < slotsLen; i++ {
-		tw.slots.Value = make(map[interface{}]struct{})
+		tw.slots.Value = newBucket()
 		tw.slots = tw.slots.Next()
 	}
 
@@ -80,41 +98,45 @@ func (tw *TimeWheel) ticksTillDie() {
 		case task := <-tw.addChan:
 			if lastBucket, ok := tw.lastBucket[task]; ok {
 				// pre bucket's life is longest
-				if tw.slots.Prev().Value == lastBucket {
+				if tw.slots.Prev().Value.(*Bucket) == lastBucket {
 					continue
 				}
 
 				// delete prev task in time wheeling
-				delete(lastBucket.(map[interface{}]struct{}), task)
+				lastBucket.deleteTask(task)
 				delete(tw.lastBucket, task)
 			}
 
-			// save task in current bucket
-			tw.slots.Prev().Value.(map[interface{}]struct{})[task] = struct{}{}
-			tw.lastBucket[task] = tw.slots.Value
+			// save task in longest life's bucket
+			tw.slots.Prev().Value.(*Bucket).addTask(task)
+			tw.lastBucket[task] = tw.slots.Prev().Value.(*Bucket)
 
 		case task := <-tw.delChan:
 			if lastBucket, ok := tw.lastBucket[task]; ok {
 				// delete prev task in time wheeling
-				delete(lastBucket.(map[interface{}]struct{}), task)
+				lastBucket.deleteTask(task)
 				delete(tw.lastBucket, task)
 			}
 
 		case <-ticker.C:
-			// stop tasks' life
-			for task, _ := range tw.slots.Value.(map[interface{}]struct{}) {
-				delete(tw.slots.Value.(map[interface{}]struct{}), task)
-				delete(tw.lastBucket, task)
-				tw.onTick(task)
-			}
-
 			if tw.debugPrint {
 				n := 0
-				tw.firstSlot.Do(func(value interface{}) {
-					bucket := value.(map[interface{}]struct{})
-					fmt.Printf("[%v] len = %v \n", n, len(bucket))
+				tw.firstSlot.Do(func(bucketInterface interface{}) {
+					bucket := bucketInterface.(*Bucket)
+					symbol := ""
+					if bucket == tw.slots.Value.(*Bucket) {
+						symbol = "<-"
+					}
+					fmt.Printf("[%v] len = %v %v\n", n, len(bucket.tasks), symbol)
 					n += 1
 				})
+			}
+
+			// stop tasks' life
+			for task, _ := range tw.slots.Value.(*Bucket).tasks {
+				tw.slots.Value.(*Bucket).deleteTask(task)
+				delete(tw.lastBucket, task)
+				tw.onTick(task)
 			}
 
 			tw.slots = tw.slots.Next()
@@ -135,18 +157,18 @@ func serverConn(conn net.Conn) {
 
 func main() {
 
-	tw := New(10, time.Second*1, func(task interface{}) {
-		fmt.Println(task)
-	})
-	tw.debugPrint = true
-
-	go func(tw *TimeWheel) {
-		tw.ticksTillDie()
-	}(tw)
-
-	tw.add("123")
-
-	time.Sleep(time.Second * 1000)
+	//tw := New(10, time.Second*1, func(task interface{}) {
+	//
+	//})
+	//tw.debugPrint = true
+	//
+	//go func(tw *TimeWheel) {
+	//	tw.ticksTillDie()
+	//}(tw)
+	//
+	//tw.add("123")
+	//
+	//time.Sleep(time.Second * 1000)
 	//arg := os.Args
 	//if len(arg) < 2 {
 	//	fmt.Printf("Usage:\n %v listenaddr\nExample:\n %v :10001\n", arg[0], arg[0])
