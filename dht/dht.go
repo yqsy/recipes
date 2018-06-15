ff48a76c8940ddabde10e8af09560a65bfe790e7
yqsy <yqsy021@126.com>
2018-06-15 11:43:35 +0800

    插入数据库代码Ok

--- dht/dht.go
+++ dht/dht.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/Sirupsen/logrus"
 	"github.com/yqsy/recipes/dht/helpful"
 	"github.com/yqsy/recipes/dht/dht"
 	"github.com/gin-gonic/gin"
 	"github.com/yqsy/recipes/dht/inspector"
+	"os"
+	"database/sql"
+	"fmt"
 )
 
 const (
@@ -25,11 +29,31 @@ func main() {
 	go func() {
 		uniqDict := make(map[string]struct{})
 
+		mysqlpassword := os.Getenv("MYSQL_PASSWORD")
+		db, err := sql.Open("mysql", fmt.Sprintf("root:%v@/dht?charset=utf8", mysqlpassword))
+
+		if err != nil {
+			panic(err)
+		}
+
 		// TODO d.Ins.HashInfoNumberAll read from db
 		for {
 			hashInfo := <-d.HashInfoChan
 			if _, ok := uniqDict[hashInfo]; !ok {
 				uniqDict[hashInfo] = struct{}{}
+
+				stmt, err := db.Prepare("insert hashinfos set hashinfo=?")
+				if err != nil {
+					logrus.Warnf("inser error: %v", err)
+				}
+
+				_, err = stmt.Exec(hashInfo)
+
+				if err != nil {
+					logrus.Warnf("exec error: %v", err)
+				}
+
+				stmt.Close()
 
 				d.Ins.SafeDo(func() {
 					d.Ins.HashInfoNumberSinceStart += 1
