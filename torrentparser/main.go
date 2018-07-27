e8e57bb73324451d17ad15c9717515d1cca42e3f
yqsy <yqsy021@126.com>
2018-07-27 15:52:30 +0800

    代码要重构,这个错误处理太恶心了

--- torrentparser/main.go
+++ torrentparser/main.go
@@ -11,6 +11,7 @@ import (
 	"io/ioutil"
 	"crypto/sha1"
 	"encoding/hex"
+	"path"
 )
 
 var usage = `Usage:
@@ -20,6 +21,73 @@ var usage = `Usage:
 type TorrentMeta struct {
 	MetaWithOutPieces map[string]interface{}
 	Pieces            string
+}
+
+// get whole path
+func CombinePath(pathSlice []interface{}, rootPath string) (string, error) {
+	if len(pathSlice) < 1 {
+		return "", errors.New(".torrent invalid")
+	}
+
+	wholePath := rootPath
+
+	for i := 0; i < len(pathSlice); i++ {
+		if node, ok := pathSlice[i].(string); !ok {
+			return "", errors.New(".torrent invalid")
+		} else {
+			wholePath = path.Join(wholePath, node)
+		}
+	}
+
+	return wholePath, nil
+}
+
+
+
+func ConvertSingleFile(singleFile map[string]interface{}, rootPath string, basedOffset int) (interface{},error){
+
+}
+
+//    {
+//        "length": 2342023084,
+//        "path": [
+//            "The.Avengers.2012.复仇者联盟.双语字幕.HR-HDTV.AC3.1024X576.x264-人人影视制作.mkv"
+//        ]
+//    },
+// 转换成
+//    {
+//      "path": "复仇者联盟DVD包/The.Avengers.2012.复仇者联盟.双语字幕.HR-HDTV.AC3.1024X576.x264-人人影视制作.mkv",
+//      "name": "The.Avengers.2012.复仇者联盟.双语字幕.HR-HDTV.AC3.1024X576.x264-人人影视制作.mkv",
+//      "length": 2342023084,
+//      "offset": 0
+//    },
+func ConvertFileObject(torrentInfoMap map[string]interface{}) (map[string]interface{}, error) {
+	if rootPath, ok := torrentInfoMap["name"]; !ok || reflect.TypeOf(rootPath).Kind() != reflect.String {
+		return nil, errors.New(".torrent invalid")
+	} else {
+
+		if files, ok := torrentInfoMap["files"]; !ok || reflect.TypeOf(files).Kind() != reflect.Slice {
+			return nil, errors.New(".torrent invalid")
+		} else {
+			filesSlice := files.([]interface{})
+
+			basedOffset := 0
+
+			for i := 0; i < len(filesSlice); i++ {
+
+				// each file object
+				singleFile := filesSlice[i]
+				if reflect.TypeOf(singleFile).Kind() != reflect.Map {
+					return nil, errors.New(".torrent invalid")
+				}
+
+
+				convertedSingleFile,err := ConvertSingleFile(singleFile.(map[string]interface{}))
+
+
+			}
+		}
+	}
 }
 
 // parse (bencode) to TorrentMeta
@@ -43,7 +111,10 @@ func ParseTorrentBytes(torrentByte []byte) (*TorrentMeta, error) {
 				return nil, errors.New(".torrent invalid")
 			} else {
 
+				// pieces 不好直接显示,所以拆成了两部分
 				torrentInfoMap["pieces"] = ""
+
+				// files数组 1.path 2.name 3.offset
 
 				torrentMeta := &TorrentMeta{
 					MetaWithOutPieces: torrentDecoded.(map[string]interface{}),
@@ -57,6 +128,7 @@ func ParseTorrentBytes(torrentByte []byte) (*TorrentMeta, error) {
 
 // https://stackoverflow.com/questions/2572521/extract-the-sha1-hash-from-a-torrent-file
 // get "info" : { }
+// used to calc hash (sha1)
 func ExtractTorrentBytes(torrentByte []byte) ([]byte, error) {
 	if torrentDecoded, err := bencode.Decode(string(torrentByte)); err != nil || reflect.TypeOf(torrentDecoded).Kind() != reflect.Map {
 		return nil, errors.New(".torrent invalid")
@@ -94,9 +166,9 @@ func main() {
 			panic(err)
 		} else {
 			// print json pretty with out Pieces bytes
-			//fmt.Printf("%v\n", string(prettyJSON.Bytes()))
+			fmt.Printf("%v\n", string(prettyJSON.Bytes()))
 
-			// print pieces
+			// prinz	t pieces
 			//pieceLen := len(torrentMeta.Pieces)
 			//if pieceLen%20 != 0 {
 			//	panic("invalid pieceLen")
