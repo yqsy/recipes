975a6ca922edc45709ce7d978cf10699bbd64995
yqsy <yqsy021@126.com>
2018-07-28 20:13:15 +0800

    再调整一下顺序

--- torrentparser/main.go
+++ torrentparser/main.go
@@ -1,146 +1,22 @@
 package main
 
 import (
-	"github.com/yqsy/recipes/dht/bencode"
-	"reflect"
-	"github.com/pkg/errors"
-	"encoding/json"
-	"bytes"
 	"fmt"
 	"os"
 	"io/ioutil"
+	"github.com/yqsy/recipes/dht/bencode"
+	"github.com/yqsy/typechekcer"
+	"reflect"
+	"bytes"
+	"encoding/json"
 	"crypto/sha1"
+	"errors"
 	"encoding/hex"
-	"path"
 )
 
 var usage = `Usage:
 %v .torrent(file)
 `
-
-type TorrentMeta struct {
-	MetaWithOutPieces map[string]interface{}
-	Pieces            string
-}
-
-// get whole path
-func CombinePath(pathSlice []interface{}, rootPath string) (string, error) {
-	if len(pathSlice) < 1 {
-		return "", errors.New(".torrent invalid")
-	}
-
-	wholePath := rootPath
-
-	for i := 0; i < len(pathSlice); i++ {
-		if node, ok := pathSlice[i].(string); !ok {
-			return "", errors.New(".torrent invalid")
-		} else {
-			wholePath = path.Join(wholePath, node)
-		}
-	}
-
-	return wholePath, nil
-}
-
-
-
-func ConvertSingleFile(singleFile map[string]interface{}, rootPath string, basedOffset int) (interface{},error){
-
-}
-
-//    {
-//        "length": 2342023084,
-//        "path": [
-//            "The.Avengers.2012.复仇者联盟.双语字幕.HR-HDTV.AC3.1024X576.x264-人人影视制作.mkv"
-//        ]
-//    },
-// 转换成
-//    {
-//      "path": "复仇者联盟DVD包/The.Avengers.2012.复仇者联盟.双语字幕.HR-HDTV.AC3.1024X576.x264-人人影视制作.mkv",
-//      "name": "The.Avengers.2012.复仇者联盟.双语字幕.HR-HDTV.AC3.1024X576.x264-人人影视制作.mkv",
-//      "length": 2342023084,
-//      "offset": 0
-//    },
-func ConvertFileObject(torrentInfoMap map[string]interface{}) (map[string]interface{}, error) {
-	if rootPath, ok := torrentInfoMap["name"]; !ok || reflect.TypeOf(rootPath).Kind() != reflect.String {
-		return nil, errors.New(".torrent invalid")
-	} else {
-
-		if files, ok := torrentInfoMap["files"]; !ok || reflect.TypeOf(files).Kind() != reflect.Slice {
-			return nil, errors.New(".torrent invalid")
-		} else {
-			filesSlice := files.([]interface{})
-
-			basedOffset := 0
-
-			for i := 0; i < len(filesSlice); i++ {
-
-				// each file object
-				singleFile := filesSlice[i]
-				if reflect.TypeOf(singleFile).Kind() != reflect.Map {
-					return nil, errors.New(".torrent invalid")
-				}
-
-
-				convertedSingleFile,err := ConvertSingleFile(singleFile.(map[string]interface{}))
-
-
-			}
-		}
-	}
-}
-
-// parse (bencode) to TorrentMeta
-// {
-//     "info": {
-//         "pieces": ""
-//     }
-// }
-func ParseTorrentBytes(torrentByte []byte) (*TorrentMeta, error) {
-	if torrentDecoded, err := bencode.Decode(string(torrentByte)); err != nil || reflect.TypeOf(torrentDecoded).Kind() != reflect.Map {
-		return nil, errors.New(".torrent invalid")
-	} else {
-
-		if torrentInfo, ok := torrentDecoded.(map[string]interface{})["info"]; !ok || reflect.TypeOf(torrentInfo).Kind() != reflect.Map {
-			return nil, errors.New(".torrent invalid")
-		} else {
-
-			torrentInfoMap := torrentInfo.(map[string]interface{})
-
-			if torrentPieces, ok := torrentInfoMap["pieces"]; ! ok || reflect.TypeOf(torrentPieces).Kind() != reflect.String {
-				return nil, errors.New(".torrent invalid")
-			} else {
-
-				// pieces 不好直接显示,所以拆成了两部分
-				torrentInfoMap["pieces"] = ""
-
-				// files数组 1.path 2.name 3.offset
-
-				torrentMeta := &TorrentMeta{
-					MetaWithOutPieces: torrentDecoded.(map[string]interface{}),
-					Pieces:            torrentPieces.(string),
-				}
-				return torrentMeta, nil
-			}
-		}
-	}
-}
-
-// https://stackoverflow.com/questions/2572521/extract-the-sha1-hash-from-a-torrent-file
-// get "info" : { }
-// used to calc hash (sha1)
-func ExtractTorrentBytes(torrentByte []byte) ([]byte, error) {
-	if torrentDecoded, err := bencode.Decode(string(torrentByte)); err != nil || reflect.TypeOf(torrentDecoded).Kind() != reflect.Map {
-		return nil, errors.New(".torrent invalid")
-	} else {
-		if torrentInfo, ok := torrentDecoded.(map[string]interface{})["info"]; !ok || reflect.TypeOf(torrentInfo).Kind() != reflect.Map {
-			return nil, errors.New(".torrent invalid")
-		} else {
-			infoEncoded := []byte(bencode.Encode(torrentInfo))
-			return infoEncoded, nil
-		}
-	}
-}
 
 func main() {
 	arg := os.Args
@@ -152,42 +28,149 @@ func main() {
 		return
 	}
 
+	rawFlag := false
+	if len(arg) > 2 {
+		if arg[2] == "--raw" {
+			rawFlag = true
+		}
+	}
+
 	torrentByte, err := ioutil.ReadFile(arg[1])
 	if err != nil {
 		panic(err)
 	}
 
-	if torrentMeta, err := ParseTorrentBytes(torrentByte); err != nil {
-		panic(err)
+	if rawFlag {
+		rawPrint(torrentByte)
 	} else {
-		jsonRaw := []byte (bencode.Prettify(torrentMeta.MetaWithOutPieces))
-		var prettyJSON bytes.Buffer
-		if err := json.Indent(&prettyJSON, jsonRaw, "", "    "); err != nil {
-			panic(err)
-		} else {
-			// print json pretty with out Pieces bytes
-			fmt.Printf("%v\n", string(prettyJSON.Bytes()))
-
-			// prinz	t pieces
-			//pieceLen := len(torrentMeta.Pieces)
-			//if pieceLen%20 != 0 {
-			//	panic("invalid pieceLen")
-			//}
-			//fmt.Printf("piecesNum: %v\n", pieceLen/20)
-			//for i := 0; i < pieceLen/20; i += 1 {
-			//	curPiece := torrentMeta.Pieces[i*20 : i*20+20]
-			//	pieceHex := hex.EncodeToString([]byte(curPiece))
-			//	fmt.Printf("%v %v\n", i, pieceHex)
-			//}
-
-			//print hash
-			extracted, err := ExtractTorrentBytes(torrentByte)
-			if err != nil {
-				panic(err)
-			}
-			sha1Sum := sha1.Sum(extracted)
-			fmt.Printf("hash: %v\n", hex.EncodeToString(sha1Sum[:]))
-
-		}
+		deepPrint(torrentByte)
 	}
+}
+
+// 调用之前需要保证slice的每个元素都是string
+func CombinePath(paths []interface{}, rootPath string) string {
+	wholePath := rootPath
+	for i := 0; i < len(paths); i++ {
+		node := paths[i].(string)
+		wholePath = wholePath + "/" + node
+	}
+	return wholePath
+}
+
+// "  files": [
+//    {
+//        "length": 2342023084,
+//        "path": [
+//            "The.Avengers.2012.复仇者联盟.双语字幕.HR-HDTV.AC3.1024X576.x264-人人影视制作.mkv"
+//        ]
+//    },
+//    ...
+//    ]
+// 转换成
+//    {
+//      "path": "复仇者联盟DVD包/The.Avengers.2012.复仇者联盟.双语字幕.HR-HDTV.AC3.1024X576.x264-人人影视制作.mkv",
+//      "name": "The.Avengers.2012.复仇者联盟.双语字幕.HR-HDTV.AC3.1024X576.x264-人人影视制作.mkv",
+//      "length": 2342023084,
+//      "offset": 0
+//    },
+func deepPrint(torrentByte []byte) {
+	objInterface, err := bencode.Decode(string(torrentByte))
+	if err != nil {
+		panic(err)
+	}
+
+	if err := typechekcer.CheckMapValue(objInterface, "info", reflect.Map, reflect.Invalid); err != nil {
+		panic(err)
+	}
+
+	// calc hash
+	info := objInterface.(map[string]interface{})["info"].(map[string]interface{})
+	infoEncoded := []byte(bencode.Encode(info))
+	sha1Sum := sha1.Sum(infoEncoded)
+	info["sha1_hash"] = hex.EncodeToString(sha1Sum[:])
+	//fmt.Printf("hash: %v\n", hex.EncodeToString(sha1Sum[:]))
+
+	// 清空
+	info["pieces"] = ""
+
+	// 计算文件
+	if err := typechekcer.CheckMapValue(info, "files", reflect.Slice, reflect.Map); err != nil {
+		panic(err)
+	}
+
+	// 获取根目录以作组合
+	if err := typechekcer.CheckMapValue(info, "name", reflect.String, reflect.Invalid); err != nil {
+		panic(err)
+	}
+
+	// 偏移
+	var offset int
+
+	files := info["files"].([]interface{})
+	for i := 0; i < len(files); i++ {
+		err := typechekcer.CheckMapValue(files[i], "length", reflect.Int, reflect.Invalid)
+		if err != nil {
+			panic(err)
+		}
+
+		err = typechekcer.CheckMapValue(files[i], "path", reflect.Slice, reflect.String)
+		if err != nil {
+			panic(err)
+		}
+
+		file := files[i].(map[string]interface{})
+		path := file["path"].([]interface{})
+
+		if len(path) < 1 {
+			panic(errors.New("invalid path"))
+		}
+
+		// 全路径
+		wholePath := CombinePath(path, info["name"].(string))
+
+		// 文件名
+		fileName := path[len(path)-1].(string)
+
+		// 长度
+		length := file["length"].(int)
+
+		// 重写
+		file["path"] = wholePath
+		file["name"] = fileName
+		file["length"] = length
+		file["offset"] = offset
+
+		// 偏移
+		offset = offset + length
+	}
+
+	jsonRaw := []byte(bencode.Prettify(objInterface))
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, jsonRaw, "", "    "); err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", string(prettyJSON.Bytes()))
+}
+
+func rawPrint(torrentByte []byte) {
+	objInterface, err := bencode.Decode(string(torrentByte))
+	if err != nil {
+		panic(err)
+	}
+	// 把pieces提取出来,其他保留不变
+	if err := typechekcer.CheckMapValue(objInterface, "info.pieces", reflect.String, reflect.Invalid); err != nil {
+		panic(err)
+	}
+	info := objInterface.(map[string]interface{})["info"].(map[string]interface{})
+	piecesStorage := info["pieces"].(string)
+	_ = piecesStorage
+	info["pieces"] = ""
+
+	// objInterface + piecesStorage 构成了torrent文件
+	jsonRaw := []byte(bencode.Prettify(objInterface))
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, jsonRaw, "", "    "); err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", string(prettyJSON.Bytes()))
 }
---
+++ torrentparser/readme.md
@@ -0,0 +1,14 @@
+
+<!-- TOC -->
+
+- [1. 说明](#1-说明)
+
+<!-- /TOC -->
+
+<a id="markdown-1-说明" name="1-说明"></a>
+# 1. 说明
+
+参考:  
+* https://stackoverflow.com/questions/2572521/extract-the-sha1-hash-from-a-torrent-file
+* https://github.com/webtorrent/parse-torrent
+* https://github.com/themasch/node-bencode (bencode) 
