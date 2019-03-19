91146a69faed195c5c4fbb278b3b5b7969985175
yqsy <yqsy021@126.com>
2019-03-19 10:11:53 +0800

    update

---
+++ echo/echo.py
@@ -0,0 +1,22 @@
+import asyncio
+
+
+async def handle_echo(reader, writer):
+    while True:
+        data = await reader.read(32 * 1024)
+        writer.write(data)
+
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
+server = loop.run_until_complete(coro)
+
+print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+# Close the server
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
